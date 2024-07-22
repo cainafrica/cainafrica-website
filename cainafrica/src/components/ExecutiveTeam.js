c85fff402bfdc730b6e967db18ce82c7f3101cc6
Joseph Aremu <32401970+aremuolaoluwa@users.noreply.github.com>
2024-07-22 17:55:56 +0100

    Update ExecutiveTeam.js

--- cainafrica/src/components/ExecutiveTeam.js
+++ cainafrica/src/components/ExecutiveTeam.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Toolbar from './Toolbar/Toolbar.js';
 import Flippy, {FrontSide, BackSide} from 'react-flippy';
-import {TopImage, TextWrapper, TopHeaderText, TopParaText, ExecutiveMother, FlipContainer, HeadingTwo, President, VP, PublicityChair, Position, Bio} from './StyledComponents.js';
+import {TopImage, TextWrapper, TopHeaderText, TopParaText, ExecutiveMother, FlipContainer, HeadingTwo, President, VP, PublicityChair, Peace, Position, Bio} from './StyledComponents.js';
 import TeamHeader2 from '../images/Our Team/TeamHeader2.jpg';
 
 const MeetTheTeam = (props) => {
@@ -35,16 +35,17 @@ const MeetTheTeam = (props) => {
                             <President> </President>
                             
                             <Position>
-                                Oghenerukeme Asagba
+                                May Oghenerukeme Asagba
                                 <br/>
-                                <i>Founder/President</i>
+                                <i>Founder/CEO</i>
                             </Position>
                             
                         </FrontSide>
                         <BackSide style={{backgroundColor: '#f4e4ba', overflow: 'auto'}} >
                             
                             <Bio>
-                            Ruke was born and raised in Lagos, Nigeria. She has a bachelor's degree in Human Biology, Health and Society, and a minor in Global Health from Cornell University. She also holds a master’s degree in Medical Physiology from Case Western Reserve University. <br/> <br/> Thus far, Ruke has conducted research on the National Health Research Act of Zambia and conducted clinical research at University Hospitals in Cleveland, Ohio. She has also served as a student health ambassador for the HBCU wellness program, and as a tutor and mentor in different capacities. Her passion for education and healthcare is intense, which is why she founded CAIN. <br/> <br/> She envisions Africa as a continent that provides access to quality education and healthcare for all. Thus, she hopes to contribute to the fulfillment of this vision by engaging future leaders in projects that combat inequities in education and healthcare. <br/> <br/> It’s a great pleasure to have a Founder/President who leads by example and gives her best to ensure that we make a tremendous impact.
+                            May is a dynamic social impact leader with a decade-long track record of implementing transformative programs and empowering young change-makers to drive development in underserved African communities. <br/> <br/> At just 21 years old, May founded Catering to Africans In Need (CAIN), a nonprofit organization. As the Founder and CEO of CAIN, she established a FREE multi-purpose educational center in the largest community in Lagos, Nigeria, benefiting over 10,000 individuals. Furthermore, she collaborated with strategic partners to pioneer the establishment of the first school and health clinic in a remote village in central Nigeria, ensuring access to quality education and healthcare.
+                            <br/> <br/> May's commitment to service extends beyond her homeland, Nigeria. She previously served as the International Director of Ligandcorp, a startup focused on providing innovative medical devices for developing nations. Additionally, she has contributed her management expertise to aid startups in moving from ideation to implementation while optimizing operational efficiency and effective marketing strategies. As a policy analyst, May conducted extensive research and stakeholder interviews, including with the Ministry of Health and the United Nations, to inform policy decisions in Zambia. <br/> <br/> As a prominent Youth Activist, May brings a wealth of expertise to address educational challenges in underserved communities. Her grassroots activism has garnered international recognition. She has shared her insights at international summits and high-profile consultations with organizations like UNESCO. She has also given a TEDx talk and been featured on BBC, Channels TV, and Business Day. <br></br> May is a distinguished alumna of Cornell University, an Ivy League institution in the U.S., where she graduated with honors in Human Biology, Health, and Society, with a minor in Global Health. She furthered her academic pursuits at Case Western Reserve University, graduating summa cum laude with a master’s degree in Medical Physiology. <br></br>May's career spans 10+ transformative programs impacting 100K+ individuals across 10 African communities. She is truly a driving force for positive change!
                             </Bio>
 
                         </BackSide>
@@ -99,6 +100,33 @@ const MeetTheTeam = (props) => {
 
                             <Bio>
                                 Daisy is currently a Software Engineer in the greater Seattle area.<br/> <br/> She is passionate about technology, education and mentoring. She is always in pursuit of ways to enlighten herself and encourages other young people to do the same. Daisy leads our communications and operations in addition to the other hats she wears. <br/> <br/>We are beyond excited to have her on board because she has a knack for getting things done and pushing other members of the team to do so too. For her, professionalism is key and we love the way she infuses that mindset across the board.
+                            </Bio>
+
+                        </BackSide>
+                    </Flippy>
+
+                    <Flippy
+                        // flipOnHover={true}
+                        flipOnClick={true} 
+                        flipDirection="horizontal"
+                        // ref={(r) => this.flippy = r}
+                        style={{ width: '22rem', margin: '1rem', height: '45rem', cursor: 'pointer' }}>
+                        
+                        <FrontSide style={{display:'flex', flexDirection:'column', justifyContent:'space-between' }}>
+
+                            <Peace> </Peace> 
+                            <Position>
+                                Peace Odiase
+                                <br/>
+                                <i>Director of Fundraising and Global Partnerships</i>
+                            </Position>
+
+                        </FrontSide>
+                        
+                        <BackSide style={{backgroundColor: '#f4e4ba', overflow: 'auto'}}>
+
+                            <Bio>
+                            At the age of 19, Peace graduated with honors as Valedictorian from the historic Fisk University with a degree in Biology/Biological Sciences. Since then, she has relentlessly pursued her passion for uplifting underserved communities. Featured on NPR, Peace has discussed her vision for alleviating the burdens of underserved communities in Nigeria and Africa through economic development and strategic partnerships. <br/> <br/>Peace currently serves as the Director of Fundraising and Global Partnerships at CAIN and is the CEO and Co-founder of Osaze, a tech startup supporting small-scale fashion designers in Nigeria. In her spare time, she volunteers as the Director of Branding at Salt Wagon Clinic, serving minority populations in Nashville, TN. She has also collaborated with Health Departments to strategize equitable COVID-19 vaccine distribution and has been instrumental in building access to trauma care and life-saving skills in Nigeria. <br></br> An aspiring neurosurgeon and neuroscientist, Peace has over 10 years of experience in medical and computational research. She has expanded her research portfolio in Neuroscience and Microbiology, co-authoring six publications to date. Her work has significantly improved the assessment of patient risk and optimized care in clinical settings, particularly for underserved minority populations burdened by health disparities and comorbidities. <br></br> Peace's dedication to her community and her multifaceted expertise make her a highly accomplished and inspiring leader in the fields of health and community development.
                             </Bio>
 
                         </BackSide>
