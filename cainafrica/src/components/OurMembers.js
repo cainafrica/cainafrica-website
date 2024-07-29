47cd4e5294c416341e17ec1024007d9146329dc4
cainafrica <tech@cainafrica.org>
2024-07-29 09:04:35 +0100

    Update OurMembers.js

--- cainafrica/src/components/OurMembers.js
+++ cainafrica/src/components/OurMembers.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Flippy, {FrontSide, BackSide} from 'react-flippy';
-import {TopImage,TextWrapper, TopHeaderText, TopParaText, MemberMother, FlipContainerMember, HeadingTwo, Position, Bio, President, Joseph, Chinedu, Abisoye, Gbolahan, Obilana, Princess, Bamidele, Abiodun} from './StyledComponents.js';
+import {TopImage,TextWrapper, TopHeaderText, TopParaText, MemberMother, FlipContainerMember, HeadingTwo, Position, Bio, CEO, Joseph, Chinedu, Abisoye, Gbolahan, Obilana, Princess, Bamidele, Abiodun} from './StyledComponents.js';
 import Toolbar from './Toolbar/Toolbar.js';
 import TeamHeader2 from '../images/Our Team/TeamHeader2.jpg';
 
@@ -30,7 +30,7 @@ function OurMembers(props) {
                         
                         <FrontSide style={{display:'flex', flexDirection:'column', justifyContent:'space-between' }}>
 
-                            <President> </President> 
+                            <CEO> </CEO> 
                             <Position>
                                 May Oghenerukeme Asagba
                                 <br/>
