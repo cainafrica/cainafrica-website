a6fefdec9bfaed916f7aee76aeebf3428428af8f
cainafrica <tech@cainafrica.org>
2024-08-16 13:53:12 +0100

    Update OurMembers.js

--- cainafrica/src/components/OurMembers.js
+++ cainafrica/src/components/OurMembers.js
@@ -38,7 +38,7 @@ function OurMembers(props) {
 
                         </FrontSide>
                         
-                        <BackSide style={{backgroundColor: '#f4e4ba', overflow: 'auto'}}>
+                        <BackSide style={{backgroundColor: '#ff0000', overflow: 'auto'}}>
 
                             <Bio>
                             May is a dynamic social impact leader with a decade-long track record of implementing transformative programs and empowering young change-makers to drive development in underserved African communities.<br/> <br/>At just 21 years old, May founded Catering to Africans In Need (CAIN), a nonprofit organization. As the Founder and CEO of CAIN, she established a FREE multi-purpose educational center in the largest community in Lagos, Nigeria, benefiting over 10,000 individuals. Furthermore, she collaborated with strategic partners to pioneer the establishment of the first school and health clinic in a remote village in central Nigeria, ensuring access to quality education and healthcare.<br/> <br/> May's commitment to service extends beyond her homeland, Nigeria. She previously served as the International Director of Ligandcorp, a startup focused on providing innovative medical devices for developing nations. Additionally, she has contributed her management expertise to aid startups in moving from ideation to implementation while optimizing operational efficiency and effective marketing strategies. As a policy analyst, May conducted extensive research and stakeholder interviews, including with the Ministry of Health and the United Nations, to inform policy decisions in Zambia. <br/> <br/> As a prominent Youth Activist, May brings a wealth of expertise to address educational challenges in underserved communities. Her grassroots activism has garnered international recognition. She has shared her insights at international summits and high-profile consultations with organizations like UNESCO. She has also given a TEDx talk and been featured on BBC, Channels TV, and Business Day.<br/> <br/> May is a distinguished alumna of Cornell University, an Ivy League institution in the U.S., where she graduated with honors in Human Biology, Health, and Society, with a minor in Global Health. She furthered her academic pursuits at Case Western Reserve University, graduating summa cum laude with a master’s degree in Medical Physiology.<br/> <br/>May's research endeavors have yielded impactful publications, including contributions to Elsevier and the Southern African Institute for Policy Analysis and Research. Her professional achievements have earned her global recognition, including selection by UNESCO as a SDG4 Youth Leader and acknowledgment as a 2023 Young Global Changer and one of the top 20 change-makers in Nigeria in 2024.<br/> <br/>May's career spans 10+ transformative programs impacting 100K+ individuals across 10 African communities. She is truly a driving force for positive change!
@@ -65,7 +65,7 @@ function OurMembers(props) {
 
                         </FrontSide>
                         
-                        <BackSide style={{backgroundColor: '#f4e4ba', overflow: 'auto'}}>
+                        <BackSide style={{backgroundColor: '#ff0000', overflow: 'auto'}}>
 
                             <Bio>
                             Chinedu is a highly accomplished lawyer and development expert specializing in Education and Governance. With over a decade of experience driving social impact projects across two continents, he has made significant strides in the development sector.
@@ -92,7 +92,7 @@ function OurMembers(props) {
 
                         </FrontSide>
                         
-                        <BackSide style={{backgroundColor: '#f4e4ba', overflow: 'auto'}}>
+                        <BackSide style={{backgroundColor: '#ff0000', overflow: 'auto'}}>
 
                             <Bio>
                             Gbolahan is a Content Creator, Writer, and Communications Manager at CAIN Africa. His passion for storytelling and attention to detail enable him to craft compelling narratives that capture the essence of CAIN Africa's mission. <br/> <br/> Aside from his professional experience, Gbolahan is also an accomplished author, having published his first book, "The Parrot," in his final year of university. His book was recognized by the Ekiti State Ministry of Education, which led to its approval as one of the literary texts for Junior Secondary schools in the state.<br/> <br/> Gbolahan is experienced in traditional and digital media and has led successful communication campaigns that raised awareness and increased engagement with key stakeholders. He has mastered various courses in digital marketing, content creation, and social media management to complement his Bachelor's Degree in Biochemistry, making him well-equipped to excel in the field of communications and content creation. <br/> <br/>Whether it is through written or visual storytelling, Gbolahan is committed to using his skills to create a positive impact in the world.
@@ -119,7 +119,7 @@ function OurMembers(props) {
 
                         </FrontSide>
                         
-                        <BackSide style={{backgroundColor: '#f4e4ba', overflow: 'auto'}}>
+                        <BackSide style={{backgroundColor: '#ff0000', overflow: 'auto'}}>
 
                             <Bio>
                             Abisoye Olagbaiye is a distinguished graduate of the University of Lagos, holding a B.A. degree in History and Strategic Studies. As the Administrative Manager at CAIN Africa, she is dedicated to ensuring the seamless operation of daily activities and maintaining the orderliness, security, and safety of the organization and its programs. <br/> <br/> With a wealth of experience in both the nonprofit and for-profit sectors, Abisoye excels in team management, customer relations, and facility management. She recently participated in the Young Women Leadership Accelerator Programme with LEAP Leadership Institute, where she excelled and was awarded a certificate, fueling her quest for growth and her desire to continue building her leadership skills in her role at CAIN.<br/> <br/> An energetic and hardworking professional, Abisoye is adept at maintaining records and contributing to the growth and development of organizations. She is a great team player who requires minimal supervision, consistently carrying out tasks to enhance organizational objectives. <br/> <br/>Passionate about mentoring younger minds, Abisoye shares her experiences to guide the next generation to thrive and succeed. Her commitment and warm demeanor make her an invaluable asset to CAIN Africa.
@@ -146,7 +146,7 @@ function OurMembers(props) {
 
                         </FrontSide>
                         
-                        <BackSide style={{backgroundColor: '#f4e4ba', overflow: 'auto'}}>
+                        <BackSide style={{backgroundColor: '#ff0000', overflow: 'auto'}}>
 
                             <Bio>
                             Joseph Aremu holds a Bachelor's degree in Computer Science Education from Tai Solarin University of Education. His passion for education has driven him to various teaching roles, where he empowers junior and senior secondary school students with essential digital skills. With extensive experience in the tech space, Joseph has worked at renowned tech organizations across Nigeria. He was an EdTech Fellow at the Co-Creation Hub, a Technology Support Associate at Tech4Dev, and served as the Chief Operating Officer at Exquitec Education Technology. <br/> <br/> In his current role as the Tech Education Lead at CAIN, Joseph is tasked with creating innovative solutions to address educational inequality in underserved communities while administering digital skills training programs to underprivileged students.<br/> <br/> Joseph is a passionate computer science enthusiast and a results-driven software developer with expertise in HTML, CSS, PHP, and MySQL. He has a proven track record of designing, developing, and maintaining web applications, and his strong skills in system analysis ensure seamless user experiences and robust functionalities. Joseph is adept at troubleshooting, optimizing performance, and implementing security measures. As a collaborative team player with excellent problem-solving abilities, he has a passion for continuous learning and improvement.
@@ -173,7 +173,7 @@ function OurMembers(props) {
 
                         </FrontSide>
                         
-                        <BackSide style={{backgroundColor: '#f4e4ba', overflow: 'auto'}}>
+                        <BackSide style={{backgroundColor: '#ff0000', overflow: 'auto'}}>
 
                             <Bio>
                             With over 12 years of teaching experience, Oluwatosin is a dedicated educator who believes in educating students with love, creating a long-lasting impression and aiding knowledge retention. <br/> <br/> She holds an NCE in Mathematics/Computer Science and is currently studying Mathematics Education at Lagos State University (LASU). Her passion for education and leadership skills are evident in her roles as a Mathematics tutor at CAIN and Headteacher at GreatBuilders Private School. In addition to her teaching career, Oluwatosin is a product designer (UI/UX) and an avid reader and curious learner. 
@@ -200,7 +200,7 @@ function OurMembers(props) {
 
                         </FrontSide>
                         
-                        <BackSide style={{backgroundColor: '#f4e4ba', overflow: 'auto'}}>
+                        <BackSide style={{backgroundColor: '#ff0000', overflow: 'auto'}}>
 
                             <Bio>
                             Bamidele David Oluwafemi, a native of Kogi State in Nigeria's north-central region, is a dedicated professional English Tutor. He earned his Bachelor's degree in Cyber Security Science from the Federal University of Technology Minna, and driven by his passion for the English language, pursued additional certificates in English from Alison Institution. 
@@ -227,7 +227,7 @@ function OurMembers(props) {
 
                         </FrontSide>
                         
-                        <BackSide style={{backgroundColor: '#f4e4ba', overflow: 'auto'}}>
+                        <BackSide style={{backgroundColor: '#ff0000', overflow: 'auto'}}>
 
                             <Bio>
                             Princess Ashibuogwu holds a Bachelor's degree in Food and Nutrition from Delta State University, Abraka, where she graduated with a 2.1, and she is currently pursuing a Master's degree in Public Health.
@@ -255,7 +255,7 @@ function OurMembers(props) {
 
                         </FrontSide>
                         
-                        <BackSide style={{backgroundColor: '#f4e4ba', overflow: 'auto'}}>
+                        <BackSide style={{backgroundColor: '#ff0000', overflow: 'auto'}}>
 
                             <Bio>
                             Andero Abiodun is a skilled Mathematics Educator, Statistician, and Data Analyst with a deep passion for teaching and imparting knowledge. His educational journey began at Multi-Grace Primary and Secondary School in Alimosho, followed by obtaining his Senior Secondary School Certificate at Millennium Secondary School in Egbeda, Lagos.
