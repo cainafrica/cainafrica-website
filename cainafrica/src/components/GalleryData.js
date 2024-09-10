4b1c47f8df51cf16e7d17ea5eda23ce51474eedf
cainafrica <tech@cainafrica.org>
2024-09-10 17:14:09 +0100

    Update GalleryData.js

--- cainafrica/src/components/GalleryData.js
+++ cainafrica/src/components/GalleryData.js
@@ -1,48 +1,75 @@
 // import coloredlogo from '../images/coloredlogo.png'
-import JeidaClinic from '../images/ProjectsData/JEIDA clinic.jpg';
-import Akaeze from '../images/ProjectsData/AKAEZE summer program.jpg';
-import CAINTUT from '../images/ProjectsData/CAIN TUTORIAL classes.jpg';
+import JeidaClinic from '../images/Jeida/JeidaDisplayPhoto.jpg';
+import Akaeze from '../images/Akaeze/Akaeze Display Picture.png';
 import Christmas from "../images/christmas/IMG4.jpg";
-import Library from '../images/Gallery/LIBRARY/L (1).jpg'
-import Career from '../images/Gallery/CAREER/B (2).jpg'
+import Library from '../images/ProjectsData/LibraryInitiative2.jpg';
+import Career from '../images/Career Enrichment/CE Display Photo.jpg';
+import CAINTUT from "../images/Tutorial/DT3.jpg";
+import FoodDrive from "../images/ProjectsData/FoodDriveCover.png";
+import Unstuck from "../images/Unstuck/Unstuck_3.jpg";
+import ICT from "../images/ProjectsData/ICT4UCover.jpg";
+import ERC from "../images/resource_center/CPL.jpg";
 
 const GalleryData = [
- {   
+    {   
         index: 1,
+        title: "ICT4U (ICT for the Underprivileged)",
+        image: `${ICT}`,
+        link: "/gallery/Ict4u"
+    },
+    {   
+        index: 2,
         title: "Community Development For Jeida Village",
         image: `${JeidaClinic}`,
         link: "/gallery/Jeida-Clinic"
     },
-
     {   
-        index: 2,
+        index: 3,
         title: "CAIN Daily Tutorial Program",
         image: `${CAINTUT}`,
         link: "/gallery/Tutorial"
     },
     {   
-        index: 3,
+        index: 4,
         title: "Akaeze Summer Program",
         image: `${Akaeze}`,
         link: "/gallery/Akaeze-Summer-Program"
     },
     {
-   		index: 4,
+   		index: 5,
     	title: "Christmas Benevolence Project ",
     	image: `${Christmas}`,
     	link: "/gallery/Christmas",
     },
     {   
-        index: 5,
+        index: 6,
         title: "The CAIN Library Initiative",
         image: `${Library}`,
         link: "/gallery/library"
     },
     {   
-        index: 5,
+        index: 7,
         title: "Career Enrichment Program",
         image: `${Career}`,
         link: "/gallery/career"
+    },
+    {   
+        index: 8,
+        title: "Unstuck Project - Empowering Young Tech Enthusiasts",
+        image: `${Unstuck}`,
+        link: "/gallery/unstuck"
+    },
+    {   
+        index: 9,
+        title: "Monthly Food Drive: Feeding the Underprivileged",
+        image: `${FoodDrive}`,
+        link: "/gallery/Food-Drive"
+    },
+    {   
+        index: 10,
+        title: "CAIN Educational Resource Center",
+        image: `${ERC}`,
+        link: "/gallery/Resource-Center"
     },
 ]
 
