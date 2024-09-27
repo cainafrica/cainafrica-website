f52032667d29805dbcdbb593ce642de41259bd4c
cainafrica <tech@cainafrica.org>
2024-09-27 11:36:18 +0100

    Update StyledComponents.js

--- cainafrica/src/components/StyledComponents.js
+++ cainafrica/src/components/StyledComponents.js
@@ -64,7 +64,7 @@ export const TopHeaderText = styled.h2`
 
 export const TopParaText = styled.p`
   margin: 0 auto;
-  margin-top: 5rem;
+  // margin-top: 5rem;
   width: 70%;
   text-align: center;
   color: white;
