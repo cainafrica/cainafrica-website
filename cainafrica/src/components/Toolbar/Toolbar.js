ae3243fe74347f458f280830def01b43359c4d96
cainafrica <tech@cainafrica.org>
2024-09-27 12:04:40 +0100

    Update Toolbar.js

--- cainafrica/src/components/Toolbar/Toolbar.js
+++ cainafrica/src/components/Toolbar/Toolbar.js
@@ -38,7 +38,7 @@ return (
                     <span className="dropbtn">Team</span>
                     <span className="dropdown-content">
                         <Link to="executiveboard" className="dropDownNavLink" >Executive Board</Link>
-                        <Link to="ourmembers" className="dropDownNavLink" >Staff</Link>
+                        <Link to="ourmembers" className="dropDownNavLink" >Management Staff</Link>
                         <Link to="edustaff" className="dropDownNavLink" >Educational Staff</Link>
                         <Link to="beavolunteer" className="dropDownNavLink" >Volunteer with Us</Link>
                     </span>
