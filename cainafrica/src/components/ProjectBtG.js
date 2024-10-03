596849b48838981fa69121eb22b6141f6f3f9a23
cainafrica <tech@cainafrica.org>
2024-10-03 15:03:04 +0100

    Update ProjectBtG.js

--- cainafrica/src/components/ProjectBtG.js
+++ cainafrica/src/components/ProjectBtG.js
@@ -3,6 +3,7 @@ import Toolbar from "./Toolbar/Toolbar.js";
 import {
   TopImage,
   TextWrapper,
+  // TopHeaderText,
   TopParaText,
   OverallWrapper,
   PostWrapper,
@@ -29,6 +30,7 @@ const ProjectBtG = (props) => {
         <Toolbar drawerClickHandler={props.drawerToggleClickHandler} />
         {props.sideDrawer}
         <TextWrapper>
+          {/* <TopHeaderText>PROJECTS</TopHeaderText> */}
           <TopParaText>
             “SMALL ACTS, WHEN MULTIPLIED BY MILLIONS OF PEOPLE, CAN TRANSFORM
             THE WORLD.” — HOWARD ZINN
@@ -39,57 +41,43 @@ const ProjectBtG = (props) => {
         <PostWrapper>
           <PostHeader>Community Development For Jeida Village</PostHeader>
           <PostParagraph>
-            Ensuring that underprivileged communities in Africa have access to
-            adequate healthcare services will continue to be our top concern and
-            it remains one of the two pillars of the CAIN Education
-            Foundation.
+            Through our partnership with Crossbonds and The Presbyterian Church of Nigeria, we are facilitating development in Jeida. Our projects are focused on providing quality healthcare services, education, and clean water to the villagers. The project has achieved a lot in the past years, but it has also faced challenges that need to be addressed to ensure the sustainability of the projects.
             <br />
             <br />
-            Kelechi Umoga, a Yale University medical student, in collaboration
-            with CAIN Education Foundation and the Presbyterian
-            Church of Nigeria, worked on a project to create a healthcare clinic
-            in Jeida - a tiny town on the outskirts of Abuja.
+            <b>Jeida Health Centre</b>
+            <br />
+            We established the Jeida Health Centre in 2016 to provide primary healthcare services to the villagers. Since its inception, the centre has safely delivered an average of 8 children yearly with 0 mortality rate.  This is a significant achievement for the centre, considering the high infant mortality rate in rural Nigeria. 
+            The community health workers and nurses have been able to provide basic medical services to the villagers, even in the absence of a doctor.
+            <br /><br />
+            Challenges: The recent brain drain of doctors in Nigeria have a grievous consequence on our health sector and the rural areas suffer most from the blow. This, coupled with the lack of basic amenities such as electricity, water, and bad roads has made it difficult to attract a doctor to the health centre as a replacement for the one who relocated.
             <br />
             <br />
-            With grants from Davis Projects for Peace, Engaged Learning and
-            Research (ELR), and the Office of Academic Diversity Initiative
-            (both departments at Cornell University), we achieved this in the
-            summer of 2013.
+            <b>Jeida School</b>
+            <br />
+            Divine Wisdom Presbyterian School has been able to provide primary education to over 100 students in the village. They have recently expanded to providing secondary education which is a positive development for the community.
+            <br />
+            Challenges: The school lacks basic amenities such as a well-stocked library, playground, and computers, which are essential for quality education.
             <br />
             <br />
-            We started laying out our strategy and putting in place the
-            procedures for establishing a health clinic in Jeida. It was erected
-            and roofed at the end of the summer. The entire building project,
-            including the installation of fixtures and painting, is now
-            complete.
+            <b>Poultry and Fish Farm</b>
+            <br />
+            The poultry and fish farm were established to finance the school and health centre projects. Until recently, the farm has been able to generate revenue to support the health and educational projects.
+            <br />
+            Challenges: The farm is currently facing difficulties due to a hike in animal feed prices and operational lapses. This has led to a significant reduction in funding for the health and educational projects.
             <br />
             <br />
-            Kelechi's work throughout the winter break in 2014 was focused on
-            procuring medical equipment and furniture for the clinic so that it
-            could open to the public in April 2015. Knowing about the ongoing
-            work, the CAIN Education Foundation was thrilled to
-            cooperate with Kelechi to secure funds from the Gianforte Family
-            Foundation.
+           <b> Water Pump Project</b>
+           <br />
+            To provide access to a clean and decent water supply, we've dug a water pump and erected an overhead tank in the community. The water pump is functional and open to use.
+
             <br />
             <br />
-            Although the villages around Jeida were in better shape than Jeida,
-            they all had the same problem of obtaining immediate access to basic
-            health care. As a result, this newly constructed out-patient health
-            clinic in the village's core acts as a conduit for basic health care
-            to reach Jeida and the surrounding towns.
+            Our intervention in Jeida has brought about significant positive change in healthcare, education, and access to clean water for the villagers. The health centre has been a life-saving facility for mothers and children, and the school has been able to provide education to the children.
             <br />
             <br />
-            We hope to see better health outcomes in these communities in the
-            long run. The success of this effort will be judged on the
-            subsequent mortality rates from preventable diseases, which will be
-            evaluated on a regular basis.
-            <br />
-            <br />
-            In addition to creating the clinic, Volunteer community health
-            workers (CHWs) were recruited from the community and trained at a
-            health technology school in the city. These CHWs act as a link
-            between the formal health system and the community, allowing for a
-            constant flow of information between the two.
+            However, the challenges faced by the health centre, school, and poultry and fish farm threaten the sustainability of the projects. We call on the general public, philanthropists, and relevant stakeholders to support the sustainability and development of these life-changing projects by investing in the health centre, providing resources to the school, and supporting the poultry and fish farm.
+            <br /><br />
+            Together, we can ensure that the health centre is fully equipped with qualified medical personnel, the school has adequate resources, and the poultry and fish farm thrives, thus ensuring a sustainable future for the community of Jeida.
             <br />
             <br />
           </PostParagraph>
@@ -99,19 +87,19 @@ const ProjectBtG = (props) => {
           <SingleImage>
             <PostImage src={`${newJeida01}`} />
             <i style={{ padding: "1rem 0", width: "95%" }}>
-              // Ongoing humanitarian work at the clinic
+              {/* Ongoing humanitarian work at the clinic */}
             </i>
           </SingleImage>
           <SingleImage>
             <PostImage src={`${newJeida02}`} />
             <i style={{ padding: "1rem 0", width: "95%" }}>
-              // Ongoing humanitarian work at the clinic
+              {/* Ongoing humanitarian work at the clinic */}
             </i>
           </SingleImage>
           <SingleImage>
             <PostImage src={`${newJeida03}`} />
             <i style={{ padding: "1rem 0", width: "95%" }}>
-              // Ongoing humanitarian work at the clinic
+              {/* Ongoing humanitarian work at the clinic */}
             </i>
           </SingleImage>
         </MediaWrapper>
