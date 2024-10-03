7d927033d3fdc149ec9aa094b1169d3710fc4e93
cainafrica <tech@cainafrica.org>
2024-10-03 12:37:20 +0100

    Update GalleryJeida.js

--- cainafrica/src/components/GalleryJeida.js
+++ cainafrica/src/components/GalleryJeida.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Toolbar from "./Toolbar/Toolbar.js";
-import { TopImage, TextWrapper, TopHeaderText } from "./StyledComponents.js";
+import { TopImage, TextWrapper } from "./StyledComponents.js";
 import PropTypes from "prop-types";
 import Gallery from "react-grid-gallery";
 import GalleryHeader3 from "../images/Gallery/GalleryHeader3.jpg";
@@ -67,7 +67,6 @@ class Demo4 extends React.Component {
           <Toolbar drawerClickHandler={this.props.drawerToggleClickHandler} />
           {this.props.sideDrawer}
           <TextWrapper>
-            <TopHeaderText>Health Care For Jeida</TopHeaderText>
           </TextWrapper>
         </TopImage>
 
