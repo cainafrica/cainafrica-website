aceb683ad10675447ede1f09b2a449f40b3ced1c
cainafrica <tech@cainafrica.org>
2024-10-03 12:54:50 +0100

    Update GalleryData.js

--- cainafrica/src/components/GalleryData.js
+++ cainafrica/src/components/GalleryData.js
@@ -1,6 +1,6 @@
 // import coloredlogo from '../images/coloredlogo.png'
 import JeidaClinic from '../images/Jeida/JeidaCoverPhoto.jpg';
-import Akaeze from '../images/Akaeze/Akaeze Display Picture.png';
+import Akaeze from '../images/Akaeze/Akaeze Gallery.png.jpg';
 import Christmas from "../images/christmas/IMG4.jpg";
 import Library from "../images/Library/Lib1.jpg";
 import Career from '../images/Career Enrichment/CE Display Photo.jpg';
