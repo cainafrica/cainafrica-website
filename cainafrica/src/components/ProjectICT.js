bfa7963b3b1a234f4b74d92de74b1c3f9f90b171
cainafrica <tech@cainafrica.org>
2024-11-22 14:08:26 +0100

    Update ProjectICT.js

--- cainafrica/src/components/ProjectICT.js
+++ cainafrica/src/components/ProjectICT.js
@@ -22,26 +22,30 @@ const ProjectICT = (props) => {
                 <PostWrapper>
                     <PostHeader>ICT4U (ICT for Underserved Students)</PostHeader>
                     <PostParagraph>
-                        By 2030, 230 million jobs in Sub-Saharan Africa will require digital skills. Shockingly, over 60% of African students currently lack basic digital training, and only 50% of African countries include 'computer' skills in their school curriculum. These alarming statistics highlight a significant digital skills shortage, necessitating urgent action. This led to the launch of the ICT4U Program by CAIN Africa, a groundbreaking initiative aimed at empowering underprivileged African students aged 10-18 with lucrative digital skills to compete in the global job market and escape the cycle of poverty.
+                        The <a href="https://www.ifc.org/en/insights-reports/annual-report/download">International Finance Corporation</a> reports that by 2030, 230 million jobs in Sub-Saharan Africa will require digital skills, yet 90% of children in Africa graduate without basic digital competencies, according to the UNDP. Adding to this crisis, MIF reports that 89% of African students lack access to computers and data shows that only half of African countries include computer skills in their school curriculums. These staggering statistics paint a grim picture of a rapidly expanding digital divide, particularly for underprivileged youth trapped in poverty. For Africa, home to the world’s youngest population, this digital skills deficit poses a critical barrier to economic mobility and global workforce readiness.
                         <br /><br />
-                        The ICT4U Program envisions a continent where all young people, regardless of socio-economic status, are digitally literate and skilled, ready for the workplace, and capable of competing globally. This initiative serves as a catalyst to lift families out of generational poverty.
+                    This urgent digital skills gap has prompted CAIN Education Foundation to launch the ICT4U(ICT for Underserved Students) Program, a transformative         initiative focused on empowering underserved youth, particularly girls, aged 10-18, in the Alimosho community of Lagos State—the largest local government area in Lagos. This demography includes young people in school and out of school. <br /><br />
+                        The ICT4U Program seeks to build a future where all African youth, regardless of socio-economic background, are equipped with the digital literacy and high-demand technical skills needed to thrive in the global job market. Special emphasis is placed on empowering girls to break barriers, close the gender gap in technology, and create equitable opportunities.  
                         <br /><br />
                         Located within the CAIN Educational Resource Center in Alimosho, the largest community in Lagos State, the CAIN ICT Center provides a safe and conducive learning environment for students. The center is fully air-conditioned and equipped with 24-hour solar-powered CCTV cameras.
                         <br /><br />
-                        The program launched with funds we could generate through collaboration with key partners like Google and Microsoft, raising a total of $9,075, translating to 9,982,500 NGN using an average exchange rate of 1,100 NGN. These funds have been utilized for procurements and recruitments essential for the first nine months of operation, from April to December 2024.
+                        Current Status of the project- With the invaluable support of implementation partners at Google and Microsoft, we successfully raised $13,000 during the first round of fundraising in 2023 and an additional $9,075 in 2024. These funds enabled us to pilot the program at the ICT Lab of the CAIN Educational Resource Center in Alimosho, Lagos, Nigeria. Leveraging these resources, we procured the following essential items:
                         <br /><br />
                         Equipment purchases include:
                         <ul>
-                            <li>4 HP Victus 16GB/512GB Windows 11, NVIDIA Core i5 Laptops: 3,370,500 NGN</li>
-                            <li>4 JBL Headphones with noise cancellation features: 72,100 NGN</li>
-                            <li>1 Digital fireproof cabinet with compartments: 1,470,000 NGN</li>
-                            <li>1 MTN 5G Internet router: 102,210 NGN</li>
-                            <li>58” Smart television: 503,300 NGN</li>
-                            <li>TV Guard and Movable whiteboard holder: 46,620 NGN</li>
-                            <li>Installation of Inverter Afripower 1Kva 12V, Tubular Orbit 220ah 12V, Solar charge controller 60 amps MIV, 4 solar panels, and 1 battery rack: 980,000 NGN</li>
-                            <li>Logistics and transportation: 18,700 NGN</li>
+                            <li>17 workstation spaces for 16 students and 1 Instructor</li>
+                            <li>3 DCP Fire extinguishers, 1 CO2 Fire extinguisher, 2 fireball extinguishers, 7 smoke detectors and an emergency exit door</li>
+                            <li>Hiring of ICT Instructor, Cleaner and Security Guard</li>
+                            <li>24 Channel CCTV Monitoring System</li>
+                            <li>4 HP Victus 16GB/512GB Windows 11, NVIDIA Core i5 Laptops</li>
+                            <li>4 JBL Headphones with noise cancellation features</li>
+                            <li>1 Digital fireproof cabinet with compartments</li>
+                            <li>1 MTN 5G Internet router</li>
+                            <li>58” Smart television</li>
+                            <li>TV Guard and Movable whiteboard holder</li>
+                            <li>Installation of Inverter Afripower 1Kva 12V, Tubular Orbit 220ah 12V, Solar charge controller 60 amps MIV, 4 solar panels, and 1 battery rack</li>
                         </ul>
-                        <strong>The total equipment cost stands at 6,563,430 NGN. Please note that these numbers are subject to change as the project progresses, with the last update on 30th July 2024.</strong>
+                        <strong>These purchases and hires have enabled us to run the program albeit below the planned capacity of 16 students per class and 48 students per cohort.</strong>
                         <br /><br />
                         We are still in the process of completing the furnishing of the ICT4U lab with all the necessary gadgets and software. We actively seek partners and donors to join us in this project. To enable the ICT Lab to function at full capacity, we need the following:
                         <ul>
@@ -49,7 +53,7 @@ const ProjectICT = (props) => {
                             <li>Solar system to power the entire facility: Cost-efficient and climate-friendly, this would enable us to run at full capacity.</li>
                             <li>Starlink Internet installation: The fastest and most reliable internet broadband platform, ensuring steady connectivity for students.</li>
                             <li>Central CAIN Server: For centralized data storage, security, backup and recovery, network management, and compliance enforcement.</li>
-                            <li>Learning management systems and edtech-based applications: To increase efficiency and encourage full digital operations within the facility.</li>
+                            <li>Learning management systems and Edtech-based applications: To increase efficiency and encourage full digital operations within the facility.</li>
                         </ul>
                         Your support can make a significant difference in bridging the digital divide and providing underprivileged students with the skills they need for a brighter future.
                         <br /><br />
