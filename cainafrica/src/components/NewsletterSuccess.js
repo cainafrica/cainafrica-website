181c5bf27ed8400e261012eba84a107d2c278d58
cainafrica <tech@cainafrica.org>
2025-04-07 16:52:41 +0100

    Add files via upload

---
+++ cainafrica/src/components/NewsletterSuccess.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Success = () => {
+  return (
+    <div className="newsletter-container succ-container">
+      <i class="fa-solid fa-circle-check check-icon"></i>
+      <h2 className="succ-heading">
+        Thank you for subscribing to our newsletter!
+      </h2>
+      <p className="succ-paragraph">We're glad to have you on board. <br></br>Stay tuned for our updates!</p>
+    </div>
+  );
+};
+
+export default Success;
